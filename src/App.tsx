5ef6cc1926e31be2ea5184b708150a31be4fc58f
Abdulbassit Abdullahi <funabab@gmail.com>
2023-07-19 04:40:58 +0100

    init: project bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
+	return (
+		<h1 className="text-2xl w-screen h-screen flex justify-center items-center">
+			👋
+		</h1>
+	)
+}
+
+export default App
